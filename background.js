369bc7c6093fbd60487fc3068607b97029b995ea
wiidu <wiiidu315@gmail.com>
2025-03-24 16:03:22 +0800

    Fixed the time being incorrect and added the break time calculation

--- background.js
+++ background.js
@@ -4,23 +4,28 @@ let countdownSeconds = document.getElementById("clock-seconds");
 let startTime;
 
 const screenTimeElements = document.querySelectorAll(".screentime");
-let screenTime = 1200000;
+var screenTime = 1200000;
 
-let breakMinutes = document.getElementById("break-minutes");
-let breakSeconds = document.getElementById("break-seconds");
-let breakTime = 20000;
+const breakTimeElements = document.querySelectorAll(".breaktime");
+var breakTime = 20000;
 
 let timerInterval;
 let currentScreen = false;
+let alternate = false;
 
 function updateTimer() {
+    if(alternate) {
+        currentScreen = !currentScreen
+        startTime = Date.now();
+        alternate = false;
+    }
     const currentTime = Date.now();
     const elapsedTime = currentTime - startTime;
     const remainingTime = Math.max((currentScreen ? screenTime: breakTime) - elapsedTime, 0);
 
     if(remainingTime == 0) {
-        currentScreen = !currentScreen;
         //Add function for when timer ends
+        alternate = true;
     } else {
         const minutes = Math.floor(remainingTime / 60000);
         const seconds = Math.floor((remainingTime % 60000) / 1000);
@@ -31,17 +36,36 @@ function updateTimer() {
 }
 
 screenTimeElements.forEach(function(st) {
-    st.addEventListener('change', function() {
-        console.log("change");
-        var minutes = Number(document.getElementById("screen-minutes"));
-        var seconds = Number(document.getElementById("screen-seconds"));
-        screentime = (minutes * 60000) + (seconds * 1000);
+    st.addEventListener('input', function() {
+        var minutes = Number(document.getElementById("screen-minutes").value);
+        var seconds = Number(document.getElementById("screen-seconds").value);
+
+        if (isNaN(minutes) || isNaN(seconds)) {
+            console.error("Invalid input: minutes and seconds must be numbers.");
+            return;
+        }
+
+        screenTime = (minutes * 60000) + (seconds * 1000);
+    });
+})
+
+breakTimeElements.forEach(function(bt) {
+    bt.addEventListener('input', function() {
+        var minutes = Number(document.getElementById("break-minutes").value);
+        var seconds = Number(document.getElementById("break-seconds").value);
+        if (isNaN(minutes) || isNaN(seconds)) {
+            console.error("Invalid input: minutes and seconds must be numbers.");
+            return;
+        }
+
+        breakTime = (minutes * 60000) + (seconds * 1000);
+        console.log(breakTime);
     });
 })
 
 checkbox.addEventListener('change', function() {
     if (this.checked) {
-        console.log()
+        console.log(screenTime)
         startTime = Date.now();
         currentScreen = true;
         updateTimer();
